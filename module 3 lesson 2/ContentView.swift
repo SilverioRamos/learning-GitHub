d69a9d6bf7e0817674b378619be6132f5f12c03e
Silverio Ramos <ramos.web@hotmail.com>
2021-04-15 11:46:19 +0100

    Initial Commit

---
+++ module 3 lesson 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  module 3 lesson 2
+//
+//  Created by Silverio Rodrigues Ramos on 15/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ module 3 lesson 2/module_3_lesson_2App.swift
@@ -0,0 +1,17 @@
+//
+//  module_3_lesson_2App.swift
+//  module 3 lesson 2
+//
+//  Created by Silverio Rodrigues Ramos on 15/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct module_3_lesson_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
